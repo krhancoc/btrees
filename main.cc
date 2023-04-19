2d25e4a0a449c11fe442ed5234c6d27a750b4f67
Ryan Hancock <krhancoc@uwaterloo.ca>
2023-04-19 14:31:08 -0700

    Cycle counts

--- main.cc
+++ main.cc
@@ -7,6 +7,7 @@
 
 #include "buf.h"
 #include "btree.h"
+#include "rdtsc.h"
 
 uint64_t generate_unique_key()
 {
@@ -33,14 +34,34 @@ diskptr_t generate_diskptr()
 #define MAX_KEYS (1000000)
 #define MAX_CHECK_KEY (10)
 
+double total_time_insert = 0;
+double num_inserts = 0;
+
+double total_time_find = 0;
+double num_finds = 0;
+
+double total_time_delete = 0;
+double num_delete = 0;
+
+static void 
+print_stat(std::string val, double total_time, double num) {
+  double average = total_time / num;
+  printf("[%s] Average: %f, Total: %f, Num: %f\n", 
+      val.c_str(), average, total_time, num);
+}
+
 int main(int argc, char *argv[])
 {
   std::unordered_map<uint64_t, diskptr_t> keys;
   diskptr_t value;
   diskptr_t check;
+  uint64_t start, stop;
   btree tree;
   int error;
   diskptr_t ptr = allocate_blk(BLKSZ);
+  printf("Calculating clock speed\n");
+  auto clkspeed = get_clock_speed_sleep();
+
 
   error = btree_init(&tree, ptr, sizeof(ptr));
   if (error) {
@@ -56,15 +77,32 @@ int main(int argc, char *argv[])
     diskptr_t value = generate_diskptr();
 
     keys.insert({key, value});
-
+    start = rdtscp();
     btree_insert(&tree, key, &value);
+    stop = rdtscp();
+    total_time_insert += stop - start;
+    num_inserts += 1;
+
+    start = rdtscp();
     error = btree_find(&tree, key, &check);
+    stop = rdtscp();
+    total_time_find += stop - start;
+    num_finds += 1;
+
     assert(error == 0);
     assert(memcmp(&check, &value, sizeof(diskptr_t)) == 0);
 
     int checkkey = 0;
     for (auto t : keys) {
+
+      start = rdtscp();
+
       error = btree_find(&tree, t.first, &check);
+
+      stop = rdtscp();
+      total_time_find += stop - start;
+      num_finds += 1;
+
       assert(error == 0);
       assert(memcmp(&check, &t.second, sizeof(diskptr_t)) == 0);
       if (checkkey == MAX_CHECK_KEY) {
@@ -75,7 +113,14 @@ int main(int argc, char *argv[])
   }
 
   for (auto t : keys) {
+    start = rdtscp();
+
     error = btree_find(&tree, t.first, &check);
+
+    stop = rdtscp();
+    total_time_find += stop - start;
+    num_finds += 1;
+
     assert(error == 0);
     assert(memcmp(&check, &t.second, sizeof(diskptr_t)) == 0);
   }
@@ -83,7 +128,15 @@ int main(int argc, char *argv[])
   printf("Deleting keys...\n");
   while (!keys.empty()) {
     auto it = keys.begin();
+
+    start = rdtscp();
+
     btree_delete(&tree, it->first, &check);
+
+    stop = rdtscp();
+    total_time_delete += stop - start;
+    num_delete += 1;
+
     assert(memcmp(&check, &it->second, sizeof(diskptr_t)) == 0);
     bzero(&check, sizeof(diskptr_t));
     error = btree_find(&tree, it->first, &check);
@@ -92,7 +145,14 @@ int main(int argc, char *argv[])
 
     int checkkey = 0;
     for (auto t : keys) {
+      start = rdtscp();
+
       error = btree_find(&tree, t.first, &check);
+
+      stop = rdtscp();
+      total_time_find += stop - start;
+      num_finds += 1;
+
       assert(error == 0);
       assert(memcmp(&check, &t.second, sizeof(diskptr_t)) == 0);
       if (checkkey == MAX_CHECK_KEY) {
@@ -103,6 +163,10 @@ int main(int argc, char *argv[])
   }
 
   print_buf_stats();
+  printf("Operation Stats in Cycles\n");
+  print_stat("Inserts", total_time_insert, num_inserts);
+  print_stat("Deletes", total_time_delete, num_delete);
+  print_stat("Find", total_time_find, num_finds);
 
   return 0;
 }
